193bf1bffe6e6a19be051b16e2990f2dc098e49f
JoonLee6075 <79310685+JoonLee6075@users.noreply.github.com>
2021-10-01 00:18:29 +0900

    Add files via upload

---
+++ Counting String.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    Scanner sc = new Scanner(System.in);
+	    String given = sc.nextLine();
+	    
+	    int answer = given.length(); 
+	    
+	    System.out.println(answer);
+		
+	}
+}
